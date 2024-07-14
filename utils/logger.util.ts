4af6ab6d837c5469246e3d3971c641c1da10e5c7
Marcel O. <marchi22494@yahoo.de>
2024-07-14 13:08:57 +0200

    added timestamp to logs

--- utils/logger.util.ts
+++ utils/logger.util.ts
@@ -17,5 +17,34 @@ export function logInfo(msg: string) {
 }
 
 export function log(msg = "", color = Color.Default) {
-  console.log(`${color}${msg}${Color.Default}`);
+  if (msg) {
+    console.log(`[${getCurrentDateTime()}] ${color}${msg}${Color.Default}`);
+  } else {
+    console.log();
+  }
+}
+
+function getCurrentDateTime(): string {
+  const date = new Date();
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  };
+
+  const formattedParts = new Intl.DateTimeFormat(
+    "en-GB",
+    options
+  ).formatToParts(date);
+
+  const formatMap: { [key: string]: string } = {};
+  formattedParts.forEach(({ type, value }) => {
+    formatMap[type] = value;
+  });
+
+  return `${formatMap["year"]}-${formatMap["month"]}-${formatMap["day"]} ${formatMap["hour"]}:${formatMap["minute"]}:${formatMap["second"]}`;
 }
